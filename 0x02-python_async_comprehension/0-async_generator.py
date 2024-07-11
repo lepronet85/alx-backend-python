c20ae67ab169d8d120889bad34ffb6cc52d6e10c
lepronet <lepronet85@gmail.com>
2024-07-11 15:51:07 +0100

    Make file executable

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,6 +10,7 @@ from typing import Generator
 async def async_generator() -> Generator[float, None, None]:
     """
     yield a random number between 0 and 10
+    :return:
     """
     for _ in range(10):
         await asyncio.sleep(1)
